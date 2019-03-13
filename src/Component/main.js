7ef24785b2c7c36223b318af834edd663372632a
RaghebBenA <ragheb.pr.2014@gmail.com>
2019-03-13 03:58:25 +0100

    display flex

--- src/Component/main.js
+++ src/Component/main.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Head from "./Header";
-import {BaseUrl} from "./baseUrl"
+import { BaseUrl } from "./baseUrl";
 import { Card, Button } from "react-bootstrap";
 
 class Main extends Component {
@@ -11,40 +11,40 @@ class Main extends Component {
     };
   }
   componentDidMount() {
-      const array = []
+    const array = [];
     fetch(BaseUrl + "Product")
       .then((response) => response.json())
-      .then((response) =>{
+      .then((response) => {
         const cards = response.map((card) => {
-            return (
-              <Card style={{ width: "18rem" }} key={Card.id}>
-                <Card.Img style={{height: "150px"}} variant="top" src={card.imageUrl} />
-                <Card.Body>
-                  <Card.Title>{card.name}</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">
-                    {card.price}
-                  </Card.Subtitle>
-                  <Card.Text>{card.content}</Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            );
-          });
-          this.setState({
-            data: cards
-        })
-      }
-      
-      );
-      
-    
+          return (
+            <Card style={{ width: "18rem" }} key={Card.id}>
+              <Card.Img
+                style={{ height: "150px" }}
+                variant="top"
+                src={card.imageUrl}
+              />
+              <Card.Body>
+                <Card.Title>{card.name}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {card.price}
+                </Card.Subtitle>
+                <Card.Text>{card.content}</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          );
+        });
+        this.setState({
+          data: cards
+        });
+      });
   }
   render() {
-      console.log(this.state.data)
+    console.log(this.state.data);
     return (
       <React.Fragment>
         <Head />
-        {this.state.data}
+        <div className="cards">{this.state.data}</div>
       </React.Fragment>
     );
   }
